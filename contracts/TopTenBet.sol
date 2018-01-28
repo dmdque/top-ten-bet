b2724e38755babf95e04ece59d9d22abd6ddf05a
Daniel Que <dmdque@gmail.com>
2018-01-28 01:29:04 -0800

    Fix compilation errors

--- contracts/TopTenBet.sol
+++ contracts/TopTenBet.sol
@@ -17,19 +17,22 @@ contract TopTenBet {
   // could remove these in favour of simple if statement that balances are nonzero
   bool public isAliceFunded;
   bool public isBobFunded;
-  bool public areBettorsSetup;
-  bool public areOraclesSetup;
   uint public endDate;
   address[5] public oracles;
   bool[5] public haveOraclesVoted;  // still not sure about "is" naming convention for bools
-  enum VoteOption {Alice, Bob};
+  enum VoteOption {Alice, Bob}
   VoteOption[5] public oracleVotes;
+  bool public areBettorsSetup;
+  bool public isEndDateSetup;
+  bool public areOraclesSetup;
 
-   modifier onlyOwner {
-      require(msg.sender == owner);
-      _;
-   }
+  // Requires that function is being called by contract owner
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
 
+  // Sets up the bet by setting up bettors, oracles, and end date
   // need a way for them to choose which side of the bet they're on
   // -> this can be hardcoded: alice always bets outcome A
   // or an enum can be supplied in setup()
@@ -42,41 +45,32 @@ contract TopTenBet {
     address _oracle3,
     address _oracle4,
     address _oracle5,
-    uint _endDate,
+    uint _endDate
   ) public onlyOwner {
     setupBettors(_alice, _bob);
     setupOracles(_oracle1, _oracle2, _oracle3, _oracle4, _oracle5);
-    endDate = _endDate;
-    isEndDateSetup = true;
+    setupEndDate(_endDate);
   }
 
-  function isSetup() {
-    return areBettorsSetup && areOraclesSetup && isEndDateSetup;
-  }
 
-  // need better name
-  // bascially is everything ready
-  function isValid() {
-    return isSetup() && isAliceFunded && isBobFunded;
-  }
-
+  // Sets up bettors with their addresses and sets areBettorsSetup to true
   // only alice and bob can participate
-  function setupBettors(address _alice, address _bob) public {
-    require isOwner(msg.sender) {
-      alice = _alice;
-      bob = _bob;
-      areBettorsSetup = true;
-    }
+  // todo: setup charity addresses
+  function setupBettors(address _alice, address _bob) public onlyOwner {
+    alice = _alice;
+    bob = _bob;
+    areBettorsSetup = true;
   }
 
+  // Sets up oracle addresses
   // can this accept an array address[4] instead?
   function setupOracles(
     address _oracle1,
     address _oracle2,
     address _oracle3,
     address _oracle4,
-    address _oracle5,
-  ) public {
+    address _oracle5
+  ) public onlyOwner {
     // todo: check that none of these addresses are "0x0"
     // check that they're all unique (not required)
     //   actually kinda required since if there's a collision, only the
@@ -90,10 +84,27 @@ contract TopTenBet {
     areOraclesSetup = true;
   }
 
+  // Sets up bet end date
+  function setupEndDate(uint _endDate) public onlyOwner {
+    endDate = _endDate;
+    isEndDateSetup = true;
+  }
+
+  // Checks that setup is properly completed
+  function isSetup() returns (bool) {
+    return areBettorsSetup && areOraclesSetup && isEndDateSetup;
+  }
+
+  // need better name
+  // basically is everything ready
+  function isValid() returns (bool) {
+    return isSetup() && isAliceFunded && isBobFunded;
+  }
+
   // Allows bettors to make their bets
   // todo: add state variable set in setup to check that both bettors bet the same amount
   // is it good practice to return isSuccess?
-  function bet() public payable (bool isSuccess) {
+  function bet() public payable returns (bool isSuccess) {
     if (msg.sender == alice) {
       balances[alice] += msg.value;
       isAliceFunded = true;
@@ -110,7 +121,7 @@ contract TopTenBet {
   // Settles the bet between alice and bob by counting votes made by oracles
   // Returns whether the settle is a success
   // is there a way to auto trigger this once the time has elapsed?
-  function settle() (bool isSuccess) {
+  function settle() returns (bool isSuccess) {
     // Perform checks, in order of increasing cost
     if (now < endDate) {
       return false;
@@ -120,7 +131,9 @@ contract TopTenBet {
       // properly, or have the contract owner refund and cancel the contract
       return false;
     }
-    (bool _isSuccess, address winner) = determineWinner();
+    bool _isSuccess;
+    address winner;
+    (_isSuccess, winner) = determineWinner();
     if (!_isSuccess) {
       return false;
     }
@@ -138,13 +151,13 @@ contract TopTenBet {
   // Checks who the winner is
   // Returns the winner's address
   // TODO: return the winner's charity's address
-  function determineWinner() (bool isSuccess, address winner) {
+  function determineWinner() returns (bool isSuccess, address winner) {
     if (!haveAllOraclesVoted()) {
       return (false, address(0));
     }
     uint _aliceVoteCount = 0;
     uint _bobVoteCount = 0;
-    for (uint i = 0; i < oracleVotes; i++) {
+    for (uint i = 0; i < oracleVotes.length; i++) {
       if (oracleVotes[i] == VoteOption.Alice) {
         _aliceVoteCount += 1;
       } else if (oracleVotes[i] == VoteOption.Bob) {
@@ -161,7 +174,7 @@ contract TopTenBet {
 
   // Checks if all oracles voted
   // Returns whether all oracles have voted
-  function haveAllOraclesVoted() bool {
+  function haveAllOraclesVoted() returns (bool) {
     for (uint i = 0; i < haveOraclesVoted.length; i++) {
       if(!haveOraclesVoted[i]) {
         return false;
@@ -173,8 +186,10 @@ contract TopTenBet {
   // Allow oracles to vote for Alice or Bob
   // Oracles can only vote once
   // Returns whether vote was successfully recorded
-  function oracleVote(VoteOption _vote) (bool isSuccess) {
-    (bool _isContained, uint _index) = arrayVoteOptionsContains(oracleVotes, _vote);
+  function oracleVote(VoteOption _vote) returns (bool isSuccess) {
+    bool _isContained;
+    uint _index;
+    (_isContained, _index) = arrayVoteOptionsContains(oracleVotes, _vote);
     if (!_isContained) {
       return false;
     }
@@ -182,7 +197,7 @@ contract TopTenBet {
     if (_hasOracleVoted) {
       return false;
     }
-    oracleVotes[_index] = vote;
+    oracleVotes[_index] = _vote;
     haveOraclesVoted[_index] = true;
     return true;
   }
@@ -193,9 +208,12 @@ contract TopTenBet {
   // Tools
   // Checks if item is contained in array
   // Returns whether item is contained in array
-  function arrayVoteOptionsContains(VoteOption[5] _oracleVotes, VoteOption _vote) (bool isContained, uint index) {
+  function arrayVoteOptionsContains(
+    VoteOption[5] _oracleVotes,
+    VoteOption _vote
+  ) returns (bool isContained, uint index) {
     for (uint i = 0; i < _oracleVotes.length; i++) {
-      if (_oracleVotes[i] == vote) {
+      if (_oracleVotes[i] == _vote) {
         return (true, i);
       }
     }
