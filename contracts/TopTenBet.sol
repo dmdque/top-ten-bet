e9ca76809d225aff18755062343647e519eb5388
Daniel Que <dmdque@gmail.com>
2018-02-11 19:04:26 -0800

    Refactor

--- contracts/TopTenBet.sol
+++ contracts/TopTenBet.sol
@@ -1,237 +1,219 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.18;
 
 
-import 'contracts/ownership/Ownable.sol';
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
+
+// TODO:
+// - make oracles dynamic array
+// - do mappings take more storage space than 3 arrays?
 
 // Bet that top 10 market cap will shift by next year.
-// TODO:
-// - make payout go directly to charity addresses
-//   - add state variable and modify setup with charity addresses
-// Usage
-// setup() -> bet() -> bet() -> wait -> settle()
-// is Ownable
 contract TopTenBet is Ownable {
+  using SafeMath for uint;
+
+  enum VoteOption {Alice, Bob}
+  enum State {
+    Setup,
+    Fund,
+    // Implicit wait
+    Vote,
+    Payout,
+    End
+  }
+
+  struct VoteInfo {
+    bool didVote;
+    VoteOption vote;
+  }
 
   mapping (address => uint) public balances;
-  // pros of storing as address[2] bettors?
   address public alice;
   address public bob;
-  address public payoutA;
-  address public payoutB;
-  // could remove these in favour of simple if statement that balances are nonzero
-  bool public isAliceFunded;
-  bool public isBobFunded;
+  address public charityA;
+  address public charityB;
+  address[3] public oracles;
+  mapping (address => VoteInfo) public oracleVotes;
+  uint public betAmount;
   uint public endDate;
-  // this should probably be a mapping instead
-  // mapping (address => bool, VoteOption) public
-  address[5] public oracles;
-  bool[5] public haveOraclesVoted;  // still not sure about "is" naming convention for bools
-  enum VoteOption {Alice, Bob}
-  VoteOption[5] public oracleVotes;
-  bool public areBettorsSetup;
-  bool public isEndDateSetup;
-  bool public areOraclesSetup;
+  uint public expiryDate;
+  State state;
 
-  // Sets up the bet by setting up bettors, oracles, and end date
-  // need a way for them to choose which side of the bet they're on
-  // -> this can be hardcoded: alice always bets outcome A
-  // or an enum can be supplied in setup()
-  // or enum can be supplied in bet(), but would have to check it's valid ie. they're not betting the same thing
+
+  modifier onlyBettor() {
+    require(msg.sender == alice || msg.sender == bob);
+    _;
+  }
+
+  modifier onlyOracle() {
+    require(msg.sender == oracles[0] ||
+            msg.sender == oracles[1] ||
+            msg.sender == oracles[2]);
+    _;
+  }
+
+  modifier onlyAfterEndDate() {
+    require(now < endDate);
+    _;
+  }
+
+  modifier onlyState(State _state) {
+    require(state == _state);
+    _;
+  }
+
+  function advanceState() internal {
+    if(state == State.Setup) {
+      // TODO: should we assert values here?
+      state = State.Fund;
+    } else if(state == State.Fund) {
+      if(balances[alice] == betAmount &&
+           balances[bob] == betAmount) {
+        state = State.Vote;
+      }
+    } else if(state == State.Vote) {
+      for (uint i = 0; i < oracles.length; i++) {
+        if(!oracleVotes[oracles[i]].didVote) {
+          return;
+        }
+      }
+      state = State.Payout;
+    } else if(state == State.Payout) {
+      state = State.End;
+    }
+  }
+
   function TopTenBet(
     address _alice,
     address _bob,
-    address _payoutA,
-    address _payoutB,
+    address _charityA,
+    address _charityB,
     address _oracle1,
     address _oracle2,
     address _oracle3,
-    address _oracle4,
-    address _oracle5,
-    uint _endDate
+    uint _endDate,
+    uint _expiryDate,
+    uint _betAmount
   ) public onlyOwner {
-    setupBettors(_alice, _bob, _payoutA, _payoutB);
-    setupOracles(_oracle1, _oracle2, _oracle3, _oracle4, _oracle5);
-    setupEndDate(_endDate);
-  }
+    require(_alice != address(0));
+    require(_bob != address(0));
+    require(_charityA != address(0));
+    require(_charityB != address(0));
+    require(_oracle1 != address(0));
+    require(_oracle2 != address(0));
+    require(_oracle3 != address(0));
+    require(_expiryDate > _endDate);
 
-
-  // Sets up bettors with their addresses and sets areBettorsSetup to true
-  // only alice and bob can participate
-  function setupBettors(address _alice, address _bob, address _payoutA, address payoutB) public onlyOwner {
     alice = _alice;
     bob = _bob;
-    payoutA = _payoutA;
-    payoutB = _payoutB;
-    areBettorsSetup = true;
-  }
-
-  // Sets up oracle addresses
-  // can this accept an array address[4] instead?
-  function setupOracles(
-    address _oracle1,
-    address _oracle2,
-    address _oracle3,
-    address _oracle4,
-    address _oracle5
-  ) public onlyOwner {
-    // todo: check that none of these addresses are "0x0"
-    // check that they're all unique (not required)
-    //   actually kinda required since if there's a collision, only the
-    //   one that appears first will get to vote, due to sequence of ifs
-    // check that none are the same as alice or bob
+    charityA = _charityA;
+    charityB = _charityB;
     oracles[0] = _oracle1;
     oracles[1] = _oracle2;
     oracles[2] = _oracle3;
-    oracles[3] = _oracle4;
-    oracles[4] = _oracle5;
-    areOraclesSetup = true;
-  }
-
-  // Sets up bet end date
-  function setupEndDate(uint _endDate) public onlyOwner {
+    betAmount = _betAmount;
     endDate = _endDate;
-    isEndDateSetup = true;
+    expiryDate = _expiryDate;
+
+    advanceState();
   }
 
-  // Checks that setup is properly completed
-  function isSetup() returns (bool) {
-    return areBettorsSetup && areOraclesSetup && isEndDateSetup;
-  }
-
-  // need better name
-  // basically is everything ready
-  function isValid() returns (bool) {
-    return isSetup() && isAliceFunded && isBobFunded;
-  }
-
-  // Allows bettors to make their bets
-  // todo: add state variable set in setup to check that both bettors bet the same amount
-  // is it good practice to return isSuccess?
-  // todo: should I limit betting to only once?
-  function bet() public payable returns (bool isSuccess) {
+  // Funds contract with bettor's bet
+  function fund()
+    public
+    payable
+    onlyBettor
+    onlyState(State.Fund)
+  {
+    require(msg.value == betAmount);
+    require(balances[msg.sender] == 0);
     if (msg.sender == alice) {
-      balances[alice] += msg.value;
-      isAliceFunded = true;
-      return true;
+      balances[alice] = msg.value;
+      advanceState();
     } else if (msg.sender == bob) {
-      balances[bob] += msg.value;
-      isBobFunded = true;
-      return true;
-    } else {
-      return false;
+      balances[bob] = msg.value;
+      advanceState();
     }
-  }
-
-  // Settles the bet between alice and bob by counting votes made by oracles
-  // Returns whether the settle is a success
-  // is there a way to auto trigger this once the time has elapsed?
-  function settle() returns (bool isSuccess) {
-    // Perform checks, in order of increasing cost
-    if (now < endDate) {
-      return false;
-    }
-    if (!isValid()) {
-      // in this case, alice and bob should check to ensure things are set up
-      // properly, or have the contract owner refund and cancel the contract
-      return false;
-    }
-    bool _isSuccess;
-    address winner;
-    (_isSuccess, winner) = determineWinner();
-    if (!_isSuccess) {
-      return false;
-    }
-    if (winner == alice) {
-      payoutA.transfer(this.balance);
-      balances[alice] = 0;
-      balances[bob] = 0;
-      return true;
-    } else if (winner == bob) {
-      payoutB.transfer(this.balance);
-      balances[alice] = 0;
-      balances[bob] = 0;
-      return true;
-    }
-  }
-
-  // Checks who the winner is
-  // Returns the winner's address
-  // TODO: return the winner's charity's address
-  function determineWinner() returns (bool isSuccess, address winner) {
-    // TODO: doesn't necessarily need all votes if a majority is already reached
-
-    if (!haveAllOraclesVoted()) {
-      return (false, address(0));
-    }
-    uint _aliceVoteCount = 0;
-    uint _bobVoteCount = 0;
-    for (uint i = 0; i < oracleVotes.length; i++) {
-      if (oracleVotes[i] == VoteOption.Alice) {
-        _aliceVoteCount += 1;
-      } else if (oracleVotes[i] == VoteOption.Bob) {
-        _bobVoteCount += 1;
-      }
-    }
-    // Assumes there can be no ties since there are an odd number of oracles
-    if (_aliceVoteCount > _bobVoteCount) {
-      return (true, alice);
-    } else if (_bobVoteCount > _aliceVoteCount) {
-      return (true, bob);
-    }
-  }
-
-  // Checks if all oracles voted
-  // Returns whether all oracles have voted
-  function haveAllOraclesVoted() returns (bool) {
-    for (uint i = 0; i < haveOraclesVoted.length; i++) {
-      if(!haveOraclesVoted[i]) {
-        return false;
-      }
-    }
-    return true;
   }
 
   // Allow oracles to vote for Alice or Bob
   // Oracles can only vote once
-  // Returns whether vote was successfully recorded
-  function oracleVote(VoteOption _vote) returns (bool isSuccess) {
-    bool _isContained;
-    uint _index;
-    // this can be simplified with a mapping from oracles to whether they voted
-    // and the vote
-    (_isContained, _index) = arrayVoteOptionsContains(oracles, msg.sender);
-    if (!_isContained) {
-      return false;
-    }
-    bool _hasOracleVoted = haveOraclesVoted[_index];
-    if (_hasOracleVoted) {
-      return false;
-    }
-    oracleVotes[_index] = _vote;
-    haveOraclesVoted[_index] = true;
-    return true;
+  function oracleVote(VoteOption _vote)
+    public
+    onlyAfterEndDate
+    onlyOracle
+    onlyState(State.Vote)
+  {
+    require(!oracleVotes[msg.sender].didVote);
+    oracleVotes[msg.sender] = VoteInfo(true, _vote);
+
+    advanceState();
   }
 
-  // TODO: kill and refund functions
-  // For refund function, keep in mind that this.balance can be nonzero if
-  // someone sent funds to this contract to avoid them being locked up forever,
-  // return them to owner
-
-  // --
-  // Tools
-  // Checks if item is contained in array
-  // Returns whether item is contained in array
-  function arrayVoteOptionsContains(
-    address[5] _oracles,
-    address _oracle
-  ) returns (bool isContained, uint index) {
-    for (uint i = 0; i < _oracles.length; i++) {
-      if (_oracles[i] == _oracle) {
-        return (true, i);
+  // Returns the winner
+  function determineWinner() internal view returns (address winner) {
+    uint _aliceVoteCount = 0;
+    uint _bobVoteCount = 0;
+    for (uint i = 0; i < oracles.length; i++) {
+      address currentOracle = oracles[i];
+      if (oracleVotes[currentOracle].vote == VoteOption.Alice) {
+        _aliceVoteCount.add(1);
+      } else if (oracleVotes[currentOracle].vote == VoteOption.Bob) {
+        _bobVoteCount.add(1);
       }
     }
-    return (false, 0);  // is it possible to not return a value, and have it default to zero-values? ie. return (false, )
+    // Outcome is binary since there's an odd number of oracles
+    if (_aliceVoteCount > _bobVoteCount) {
+      return alice;
+    } else {
+      return bob;
+    }
+  }
+
+  // Settles the bet between alice and bob by counting votes made by oracles
+  // Returns whether the payout is a success
+  // is there a way to auto trigger this once the time has elapsed?
+  function payout()
+    public
+    onlyState(State.Payout)
+  {
+    address winner = determineWinner();
+    if (winner == alice) {
+      charityA.transfer(this.balance);
+      balances[alice] = 0;
+      balances[bob] = 0;
+      advanceState();
+    } else if (winner == bob) {
+      charityB.transfer(this.balance);
+      balances[alice] = 0;
+      balances[bob] = 0;
+      advanceState();
+    }
+  }
+
+  // Refunds balances to bettors
+  function abort()
+    public
+    onlyOwner
+  {
+    // Update balances before transferring
+    uint aliceAmount = balances[alice];
+    uint bobAmount = balances[bob];
+    balances[alice] = 0;
+    balances[bob] = 0;
+    alice.transfer(aliceAmount);
+    bob.transfer(bobAmount);
+    state = State.End;
+  }
+
+  // Refunds balance to bettor
+  function personalAbort() public onlyBettor {
+    require(now >= expiryDate);
+    uint amount = balances[msg.sender];
+    balances[msg.sender] = 0;
+    msg.sender.transfer(amount);
+    state = State.End;
   }
 
 }
